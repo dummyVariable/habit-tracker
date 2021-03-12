28fded045e0ce3ed2f61428db03674423e1b7d1f
dummyvariable <officialsanjivee@gmail.com>
2021-03-12 23:32:18 +0530

    created factory method for habitDB

--- model.go
+++ model.go
@@ -1,15 +1,27 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 type (
 	habitID uuid.UUID
 	taskID  uuid.UUID
 )
 
+//habitDBStore implements habitDB in-memory
 type habitDBStore struct {
 	habits map[habitID]habit
 	tasks  map[taskID]task
 
 	habitTaskMap map[habitID][]taskID
+}
+
+func newStore() habitDBStore {
+	return habitDBStore{
+		habits: make(map[habitID]habit),
+		tasks:  make(map[taskID]task),
+
+		habitTaskMap: make(map[habitID][]taskID),
+	}
 }
