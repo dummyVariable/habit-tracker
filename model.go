23176cc36c9369550f296b1240639bfb9e0051e8
dummyvariable <officialsanjivee@gmail.com>
2021-03-12 23:36:52 +0530

    updated model to use name instead of id

--- model.go
+++ model.go
@@ -2,38 +2,35 @@ package main
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type (
-	habitID uuid.UUID
-	taskID  uuid.UUID
+	habitName string
+	taskName  string
 )
 
 //habitDBStore implements habitDB in-memory
 type habitDBStore struct {
-	habits map[habitID]habit
-	tasks  map[taskID]task
+	habits map[habitName]habit
+	tasks  map[taskName]task
 
-	habitTaskMap map[habitID][]taskID
+	habitTaskMap map[habitName][]taskName
 }
 
 func newStore() habitDBStore {
 	return habitDBStore{
-		habits: make(map[habitID]habit),
-		tasks:  make(map[taskID]task),
+		habits: make(map[habitName]habit),
+		tasks:  make(map[taskName]task),
 
-		habitTaskMap: make(map[habitID][]taskID),
+		habitTaskMap: make(map[habitName][]taskName),
 	}
 }
 
 func (db *habitDBStore) addHabit(newHabit habit) error {
-	ID := uuid.New()
-	newHabit.ID = ID
+
 	newHabit.adoptionRate = 0
 	newHabit.startedAt = time.Now()
 
-	db.habits[habitID(newHabit.ID)] = newHabit
+	db.habits[habitName(newHabit.name)] = newHabit
 	return nil
 }
