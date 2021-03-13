cb081a983a301e242de57351e35131df714f3c3d
dummyvariable <officialsanjivee@gmail.com>
2021-03-13 17:11:32 +0530

    updated habit struct to maintain habit streak count

--- habit.go
+++ habit.go
@@ -13,12 +13,12 @@ type task struct {
 	lastBestAt  time.Time
 }
 
-//habit contains the habit name, habit tasks and the habit's adoption rate, and
-//habit started.
+//habit contains the habit name, and the habit's adoption rate, habit started and current streak.
 type habit struct {
 	name         string
 	adoptionRate int
 	startedAt    time.Time
+	streak       int
 }
 
 //habitDB is implemented by datastore for the habits
