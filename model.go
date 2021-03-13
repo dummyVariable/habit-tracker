65fa652c801b47bb492c7145cd5a412736670e08
dummyvariable <officialsanjivee@gmail.com>
2021-03-13 13:27:34 +0530

    created removeTask method

--- model.go
+++ model.go
@@ -71,3 +71,23 @@ func (db *habitDBStore) addTask(habit string, newTask task) error {
 
 	return nil
 }
+func (db *habitDBStore) removeTask(habit, task taskName) error {
+
+	var index int
+
+	if !contains(db.habitTaskMap[habitName(habit)], task) {
+		return errors.New("Task not exists")
+	}
+
+	for i, key := range db.habitTaskMap[habitName(habit)] {
+		if key == task {
+			index = i
+			break
+		}
+
+	}
+	db.habitTaskMap[habitName(habit)] = append(db.habitTaskMap[habitName(habit)][:index], db.habitTaskMap[habitName(habit)][index+1:]...)
+	delete(db.tasks, task)
+	return nil
+
+}
