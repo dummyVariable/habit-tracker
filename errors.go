68b38bb701a5dd839df37861f9abda546513e767
dummyvariable <officialsanjivee@gmail.com>
2021-03-19 21:40:04 +0530

    removed task errors

--- errors.go
+++ errors.go
@@ -5,6 +5,4 @@ import "errors"
 var (
 	ErrHabitNotExists     = errors.New("Habit not exists")
 	ErrHabitAlreadyExists = errors.New("Habit already exists")
-	ErrTaskNotExists      = errors.New("Task not exists")
-	ErrTaskAlreadyExists  = errors.New("Task already exists")
 )
