3a7dfb1f10660e820f435d89ce5933bacc41f5e3
dummyvariable <officialsanjivee@gmail.com>
2021-03-20 20:39:25 +0530

    updated errors and created checkErr function

--- errors.go
+++ errors.go
@@ -1,9 +1,19 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"log"
+)
 
 var (
 	ErrHabitNotExists        = errors.New("Habit not exists")
 	ErrHabitAlreadyExists    = errors.New("Habit already exists")
 	ErrJSONFileAlreadyExists = errors.New("JSON file already exists")
+	ErrJSONFileNotExists     = errors.New("Json file not exists")
 )
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
