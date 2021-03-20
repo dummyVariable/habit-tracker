27a60ee35e4889d3e4d35fa37cbc444c2b8c6565
dummyvariable <officialsanjivee@gmail.com>
2021-03-20 19:58:03 +0530

    added util function to check and create json file

--- store.go
+++ store.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"os"
+)
+
 type habitJSONStore struct {
 	dataFilename string
 }
@@ -8,4 +12,28 @@ func newJSONStore() habitJSONStore {
 	return habitJSONStore{
 		dataFilename: "habit.json",
 	}
+}
+
+func isJSONExists(filename string) bool {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func createJSONFile(filename string) error {
+	if isJSONExists(filename) {
+		return ErrJSONFileAlreadyExists
+	}
+
+	f, err := os.Create(filename)
+	defer f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
