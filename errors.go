18404d278602fa7969e8f37b82706a22d2baf9a9
dummyvariable <officialsanjivee@gmail.com>
2021-03-20 19:58:32 +0530

    added errors realted to json issues

--- errors.go
+++ errors.go
@@ -3,6 +3,7 @@ package main
 import "errors"
 
 var (
-	ErrHabitNotExists     = errors.New("Habit not exists")
-	ErrHabitAlreadyExists = errors.New("Habit already exists")
+	ErrHabitNotExists        = errors.New("Habit not exists")
+	ErrHabitAlreadyExists    = errors.New("Habit already exists")
+	ErrJSONFileAlreadyExists = errors.New("JSON file already exists")
 )
