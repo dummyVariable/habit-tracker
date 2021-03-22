243dc644d7af08b95076f45129ea21ef25a9adde
dummyvariable <officialsanjivee@gmail.com>
2021-03-22 19:05:30 +0530

    adding json info for habit attributes

--- habit.go
+++ habit.go
@@ -6,12 +6,12 @@ import (
 
 //habit contains the habit name, and the habit's adoption rate, habit started and current streak.
 type habit struct {
-	name             string
-	description      string
-	adoptionRate     int
-	createdAt        time.Time
-	streak           int
-	lastCompletionAt time.Time
+	Name             string    `json:"name"`
+	Description      string    `json:"description"`
+	AdoptionRate     int       `json:"adoptionRate"`
+	CreatedAt        time.Time `json:"createdAt"`
+	Streak           int       `json:"streak"`
+	LastCompletionAt time.Time `json:"lastCompletionAt`
 }
 
 //habitDB is implemented by datastore for the habits
